e202e2163a91fe46df80f9bdacf78fc6422b0118
Juti Buican <juti.buican@bottomline.com>
2022-10-17 20:26:46 +0100

    TS-drag&drop: create projectItem class to handle the rendering each list

--- src/app.ts
+++ src/app.ts
@@ -154,6 +154,28 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+// PROJECTITEM CLASS
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project; 
+
+  constructor(hostId: string, project:Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project; 
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure(): void {    
+  }
+
+  renderContent(): void {
+    this.element.querySelector('h2')!.textContent = this.project.title;  
+    this.element.querySelector('h3')!.textContent = this.project.people.toString(); 
+    this.element.querySelector('p')!.textContent = this.project.description;   
+  }
+}
+
 // PROJECT LIST CLASS
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -174,9 +196,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     // performance issue but small project is fine.
     listElm.innerHTML = "";
     for (const projectItem of this.assignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = projectItem.title;
-      listElm?.appendChild(listItem);
+      new ProjectItem(this.element.querySelector('ul')!.id, projectItem);
     }
   }
 
